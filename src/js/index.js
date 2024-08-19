694b8d08a7c67b55075390a96f8f36e0998f8b62
Ronaldo Porto <portofilhoronaldo@gmail.com>
2024-08-19 16:48:26 -0300

    first

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+const informacoes = document.querySelectorAll(".informacoes");
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desativarBotaoSelecionado();
+
+        botao.classList.add("selecionado");
+
+        esconderImagemAtiva();
+
+        imagens[indice].classList.add("ativa");
+
+        escondendoInformacoesAtivas();
+
+        informacoes[indice].classList.add("ativa");
+    })
+})
+
+function escondendoInformacoesAtivas() {
+    const informacoesAtiva = document.querySelector(".informacoes.ativa");
+    informacoesAtiva.classList.remove("ativa");
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector(".selecionado");
+
+    botaoSelecionado.classList.remove("selecionado");
+}
+
